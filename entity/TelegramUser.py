78620df1ff890040ab946ccdfba88d7f5dd2a46c
Даня <6da6nya6@gmail.com>
2023-02-05 02:19:13 +0300

    изменил файл gitignore

--- entity/TelegramUser.py
+++ entity/TelegramUser.py
@@ -1,25 +1,46 @@
 class TelegramUser:
-    def __init__(self, username , firstname, lastname,is_active, chat_id):
+    def __init__(self, username, firstname, lastname, is_active, chat_id):
         self._username = username
         self._firstname = firstname
         self._lastname = lastname
         self._is_active = is_active
         self._chat_id = chat_id
 
-    def get_user(self):
-        return print(f'Welcome: {self._username}, {self._firstname},{self._lastname}, {self._is_active}, {self._chat_id}')
+    @property
+    def username(self):
+        return self._username
 
-    def set_user(self, username, firstname, lastname, is_active, chat_id):
-        self._username = username
-        self._firstname = firstname
-        self._lastname = lastname
-        self._is_active = is_active
-        self._chat_id = chat_id
+    @username.setter
+    def username(self, name):
+        self._username = name
+
+    @property
+    def first(self):
+        return self._firstname
+
+    @first.setter
+    def first(self, first):
+        self._firstname = first
+
+    @property
+    def lastname(self):
+        return self._lastname
+
+    @lastname.setter
+    def lastname(self, last):
+        self._lastname = last
+
+    @property
+    def active(self):
+        return self._is_active
+
+    @active.setter
+    def active(self, active):
+        self._is_active = active
+
+    @property
+    def chat_id(self):
+        return self._chat_id
 
 
-user = TelegramUser("bob ","bobinski ", " "," "," ")
-user.get_user()
-user1 = user.set_user("nice","joi","division","active","hello")
-user.get_user()
-print(user)
-print(user1)
+
